1bd82dd880d5dcd102638796c1076de65a952401
Marvin Zumbado <marv_21@hotmail.com>
2016-09-10 19:12:09 -0600

    ok

--- POCO/Models/Project.cs
+++ POCO/Models/Project.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace POCO.Models
 {
-    public class Project
+    public class Project : Audit
 
     {
 
--- POCO/Models/client.cs
+++ POCO/Models/client.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace POCO.Models
 {
-    public class Client
+    public class Client : Audit
 
     {
 
